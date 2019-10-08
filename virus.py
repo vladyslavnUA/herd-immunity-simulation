94b3a792d18c8722c0518014016e601d61cd1867
Vladyslav Nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2019-10-07 19:44:34 -0700

    updated virus.py, added more virus tests

--- virus.py
+++ virus.py
@@ -14,3 +14,29 @@ def test_virus_instantiation():
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
     assert virus.mortality_rate == 0.3
+
+    malaria = Virus('malaria', 0.6, 0.1)
+    assert malaria.name == "malaria"
+    assert malaria.repro_rate == 0.6
+    assert malaria.mortality_rate == 0.1
+
+    blackDeath = Virus('blackDeath', 0.8, 0.7)
+    assert blackDeath.name == "blackDeath"
+    assert blackDeath.repro_rate == 0.8
+    assert blackDeath.mortality_rate == 0.7
+
+    chickenPox = Virus('chickenPox', 0.2, 0.275)
+    assert chickenPox.name == "chickenPox"
+    assert chickenPox.repro_rate == 0.2
+    assert chickenPox.mortality_rate == 0.275
+
+    ebola = Virus('ebola', 0.5, 0.8)
+    assert ebola.name == "ebola"
+    assert ebola.repro_rate == 0.5
+    assert ebola.mortality_rate == 0.8
+
+    measles = Virus('measles', 0.6, 0.9)
+    assert measles.name == "measles"
+    assert measles.repro_rate == 0.6
+    
+    assert measles.mortality_rate == 0.9
